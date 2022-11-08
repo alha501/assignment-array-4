687baadc87b1cb68567fe970143a15c1490510d5
alha501 <113909973+alha501@users.noreply.github.com>
2022-11-07 21:34:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let number = 0
+input.onButtonPressed(Button.A, function () {
+    number += 1
+    basic.showNumber(number)
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    number += -1
+    basic.showNumber(number)
+})
+input.onPinPressed(TouchPin.P1, function () {
+	
+})
+basic.forever(function () {
+	
+})
