912010019210f879248e88b87847865239704e46
alha501 <113909973+alha501@users.noreply.github.com>
2022-11-10 09:53:32 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
-let number = 0
 input.onButtonPressed(Button.A, function () {
-    number += 1
+    number += -1
     basic.showNumber(number)
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    list.push(number)
 })
 input.onButtonPressed(Button.B, function () {
-    number += -1
+    number += 1
     basic.showNumber(number)
 })
 input.onPinPressed(TouchPin.P1, function () {
 	
 })
-basic.forever(function () {
-	
-})
+let list: number[] = []
+let number = 0
+number = 0
+basic.showNumber(number)
+list = []
